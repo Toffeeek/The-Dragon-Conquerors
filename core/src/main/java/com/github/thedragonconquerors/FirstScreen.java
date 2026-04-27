8e0c6628ddbf56aa668e81c76e90645987858f33
Ishmam90 <ishmamkabir90@gmail.com>
2026-04-27 20:15:20 +0600

    window rendering changes

--- core/src/main/java/com/github/thedragonconquerors/FirstScreen.java
+++ core/src/main/java/com/github/thedragonconquerors/FirstScreen.java
@@ -2,31 +2,47 @@ package com.github.thedragonconquerors;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.github.thedragonconquerors.assets.AssetService;
 import com.github.thedragonconquerors.assets.MapAssets;
 
 public class FirstScreen extends ScreenAdapter {
     private final Main game;
+    private final Batch batch;
     private final AssetService assetService;
     private final Viewport viewport;
     private final OrthographicCamera camera;
+    private final OrthogonalTiledMapRenderer mapRenderer;
 
     public FirstScreen(Main game){
         this.game = game;
         this.assetService = game.getAssetService();
         this.viewport = game.getViewport();
         this.camera = game.getCamera();
+        this.batch = game.getBatch();
+        this.mapRenderer = new OrthogonalTiledMapRenderer(null, Main.UNIT_SCALE, this.batch);
     }
 
     @Override
     public void show(){
         this.assetService.load(MapAssets.MAIN);
+        this.mapRenderer.setMap(this.assetService.get(MapAssets.MAIN));
     }
 
     @Override
     public void render(float delta){
-        super.render(delta);
+        this.viewport.apply();
+        this.batch.setColor(Color.WHITE);
+        this.mapRenderer.setView(this.camera);
+        this.mapRenderer.render();
+    }
+
+    @Override
+    public void dispose() {
+        this.mapRenderer.dispose();
     }
 }
