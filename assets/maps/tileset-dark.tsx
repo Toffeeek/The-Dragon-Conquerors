100592de4628801d6708fa7e7f92dfd9bad905f2
Ishmam90 <ishmamkabir90@gmail.com>
2026-04-28 19:02:05 +0600

    map now rendereing

---
+++ assets/maps/tileset-dark.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="tileset-dark" tilewidth="256" tileheight="256" tilecount="18" columns="3">
+ <tileoffset x="0" y="10"/>
+ <image source="../../assets_raw/map/tileset-dark.png" width="887" height="1774"/>
+</tileset>
---
+++ assets/maps/tilset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="tilset" tilewidth="16" tileheight="16" spacing="40" margin="20" tilecount="12" columns="3">
+ <image source="../../assets_raw/map/tileset.png" width="192" height="256"/>
+</tileset>
