24896c7a15c0fa192de9bee549829ae56f10a78b
Ishmam90 <ishmamkabir90@gmail.com>
2026-04-28 21:22:31 +0600

    player rendering

--- core/src/main/java/com/github/thedragonconquerors/FirstScreen.java
+++ core/src/main/java/com/github/thedragonconquerors/FirstScreen.java
@@ -1,60 +1,101 @@
 package com.github.thedragonconquerors;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.github.thedragonconquerors.assets.AssetService;
 import com.github.thedragonconquerors.assets.MapAssets;
+import com.github.thedragonconquerors.core.GridManager;
 import com.github.thedragonconquerors.core.MovementSystem;
 import com.github.thedragonconquerors.core.Player;
+import com.github.thedragonconquerors.rendering.GridRenderer;
 import com.github.thedragonconquerors.rendering.PlayerRenderer;
 
 public class FirstScreen extends ScreenAdapter {
     private final Main game;
     private final Batch batch;
-    private final AssetService assetService;
+    //private final AssetService assetService;
     private final Viewport viewport;
     private final OrthographicCamera camera;
-    private final OrthogonalTiledMapRenderer mapRenderer;
-    private MovementSystem movementSystem;
+    //private final OrthogonalTiledMapRenderer mapRenderer;
+    //private MovementSystem movementSystem;
     private Player player;
     private PlayerRenderer playerRenderer;
+    private GridManager gridManager;
+    private GridRenderer gridRenderer;
 
     public FirstScreen(Main game){
         this.game = game;
-        this.assetService = game.getAssetService();
+        //this.assetService = game.getAssetService();
         this.viewport = game.getViewport();
         this.camera = game.getCamera();
         this.batch = game.getBatch();
-        this.mapRenderer = new OrthogonalTiledMapRenderer(null, Main.UNIT_SCALE, this.batch);
+        //this.mapRenderer = new OrthogonalTiledMapRenderer(null, Main.UNIT_SCALE, this.batch);
     }
 
     @Override
     public void show(){
         //build core system
-
+        gridManager = new GridManager();
 
         // Spawn player at tile(2, 2)
         player = new Player(2, 2, 5);
+        gridManager.getTile(2, 2).setOccupied(true);
 
-        this.assetService.load(MapAssets.MAIN);
-        this.mapRenderer.setMap(this.assetService.get(MapAssets.MAIN));
+        //build renderers
+        gridRenderer = new GridRenderer(gridManager);
+        playerRenderer = new PlayerRenderer();
+
+        //this.assetService.load(MapAssets.MAIN);
+        //this.mapRenderer.setMap(this.assetService.get(MapAssets.MAIN));
     }
 
     @Override
     public void render(float delta){
-        this.viewport.apply();
-        this.batch.setColor(Color.WHITE);
-        this.mapRenderer.setView(this.camera);
-        this.mapRenderer.render();
+        //update player animation
+        player.update(delta);
+
+        //clear screen
+        ScreenUtils.clear(Color.BLACK);
+
+        //render grid
+        gridRenderer.render(camera.combined);
+
+        //render player
+        playerRenderer.render(player, camera.combined);
+
+
+//        this.viewport.apply();
+//        this.batch.setColor(Color.WHITE);
+//        this.mapRenderer.setView(this.camera);
+//        this.mapRenderer.render();
+    }
+
+    //ends current turn and resets player stamina
+    private void endTurn(){
+        player.resetStamin();
+    }
+
+    @Override
+    public void resize(int width, int height){
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void hide(){
+        Gdx.input.setInputProcessor(null);
     }
 
     @Override
     public void dispose() {
-        this.mapRenderer.dispose();
+        gridRenderer.dispose();
+        playerRenderer.dispose();
+        //this.mapRenderer.dispose();
     }
 }
